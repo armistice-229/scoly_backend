b4f061b161858607d6cb5c800e9f7ea4614df281
armistice-229 <armistice095@gmail.com>
2025-08-23 17:01:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
+
 dotenv.config();
 const app = express();
 
+// Domaine de ton frontend (exemple : https://mon-frontend.com)
+const FRONTEND_URL = "https://scoly.onrender.com";
+
+app.use(cors({
+  origin: FRONTEND_URL,    // ✅ seul ce domaine est accepté
+  methods: ["GET", "POST", "PUT", "DELETE"], // méthodes autorisées
+  credentials: true        // si tu utilises cookies/token
+}));
+
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Connection à la base de données MongoDB
